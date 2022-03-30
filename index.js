421b9ec4f12fb933da36b0f76e0773f3c433f323
Dammie <bitbender1010@gmail.com>
2022-03-30 07:54:59 +0100

    reduce logo size

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// var countDownDate = new Date("April 25, 2022").getTime();
-
-// console.log(countDownDate);
-
-// var myfunc = setInterval(function() {
-//     // code goes here
-//     var now = new Date().getTime();
-//     var timeleft = countDownDate - now;
-    
-//     var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-//     var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-//     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-//     document.getElementById("days").innerHTML = days + "d "
-//     document.getElementById("hours").innerHTML = hours + "h " 
-//     document.getElementById("mins").innerHTML = minutes + "m " 
-//     document.getElementById("secs").innerHTML = seconds + "s"
-// }, 1000)
-
 
 var countDownDate = new Date("April 25, 2022").getTime();
 
